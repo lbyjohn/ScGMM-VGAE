9b2d6be70520d6532bea64d483bdcad85f0a472c
lbyjohn <105017237+lbyjohn@users.noreply.github.com>
2022-08-21 14:51:32 -0400

    Add files via upload

---
+++ ScGMM-VGAE/metrics.py
@@ -0,0 +1,32 @@
+# Following codes are to evaluate the performance of scGMM-VGAE
+# The codes are based on Eric Lin's codes
+# Lin., Liu., et al., 2022 (In preparation)
+
+import numpy as np
+from sklearn.metrics import normalized_mutual_info_score, adjusted_rand_score, f1_score
+
+nmi = normalized_mutual_info_score
+ari = adjusted_rand_score
+f1 = f1_score
+
+def acc(y_true, y_pred):
+    """
+    Calculate clustering accuracy. Require scikit-learn installed
+
+    # Arguments
+        y: true labels, numpy.array with shape `(n_samples,)`
+        y_pred: predicted labels, numpy.array with shape `(n_samples,)`
+
+    # Return
+       accuracy, in [0,1]
+    """
+    y_true = y_true.astype(np.int64)
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+    #from sklearn.utils.linear_assignment_ import linear_assignment
+    from scipy.optimize import linear_sum_assignment
+    row_ind, col_ind = linear_sum_assignment(w.max() - w)
+    return sum([w[i, j] for i, j in zip(row_ind, col_ind)]) * 1.0 / y_pred.size
---
+++ ScGMM-VGAE/preprocessing.py
@@ -0,0 +1,95 @@
+# Following codes are to pre-process the input files
+# The codes are based on Eric Lin's codes
+# Lin., Liu., et al., 2022 (In preparation)
+
+import torch
+import numpy as np
+import pickle as pkl
+import networkx as nx
+import scipy.sparse as sp
+
+def parse_index_file(filename):
+    """Parse the index file path
+
+    Args:
+        filename: The file name of the dataset
+
+    Returns:
+        _type_: List of files
+    """
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+def load_data(dataset, data_path, modified):
+    """Load the data 
+
+    Args:
+        dataset: Dataset name
+        data_path: Dataset file
+        modified: Whether the data is modified for no combination
+
+    Returns:
+        _type_: Return the adjacency matrix, feature matrix and label
+    """
+    # load the data: x, tx, allx, graph
+    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
+    objects = []
+    for i in range(len(names)):
+        with open(data_path+"/ind.{}.{}".format(dataset, names[i]), 'rb') as rf:
+            u = pkl._Unpickler(rf)
+            u.encoding = 'latin1'
+            cur_data = u.load()
+            objects.append(cur_data)
+    x, y, tx, ty, allx, ally, graph = tuple(objects)
+    test_idx_reorder = parse_index_file(data_path+"/ind.{}.test.index".format(dataset))
+    test_idx_range = np.sort(test_idx_reorder)
+
+
+    if modified:
+        features = allx
+        labels = ally
+    else:
+        features = sp.vstack((allx, tx)).tolil()
+        features[test_idx_reorder, :] = features[test_idx_range, :]
+
+        labels = np.vstack((ally, ty))
+        labels[test_idx_reorder, :] = labels[test_idx_range, :]
+
+    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+
+    return adj, features, np.argmax(labels,1)
+
+
+def sparse_to_tuple(sparse_mx):
+    """Convert sparse matrix to tuple
+
+    Args:
+        sparse_mx: Sparse matrix
+
+    Returns:
+        _type_: Tuple values
+    """
+    if not sp.isspmatrix_coo(sparse_mx):
+        sparse_mx = sparse_mx.tocoo()
+    coords = np.vstack((sparse_mx.row, sparse_mx.col)).transpose()
+    values = sparse_mx.data
+    shape = sparse_mx.shape
+    return coords, values, shape
+
+def preprocess_graph(adj):
+    """Preprocess the graphs
+
+    Args:
+        adj: Adjacency matrix
+
+    Returns:
+        _type_: Normalized adjacency matrix
+    """
+    adj = sp.coo_matrix(adj)
+    adj_ = adj + sp.eye(adj.shape[0])
+    rowsum = np.array(adj_.sum(1))
+    degree_mat_inv_sqrt = sp.diags(np.power(rowsum, -0.5).flatten())
+    adj_normalized = adj_.dot(degree_mat_inv_sqrt).transpose().dot(degree_mat_inv_sqrt).tocoo()
+    return sparse_to_tuple(adj_normalized)
